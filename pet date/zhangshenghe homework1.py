f42f5d895ae6787ae16cd3ba3a009ffb4cb039df
zhang shenghe <3240170837@qq.com>
2024-12-13 22:41:56 +0800

    Delete pet date directory

--- pet date/zhangshenghe homework1.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-data = pd.read_csv('seattle_pet_licenses.csv')
-num_pets = data.shape[0]
-print(f"{num_pets}")
-num_variables = data.shape[1]
-print(f"{num_variables}")
-name_counts = data['animal_s_name'].value_counts()
-top_three_names = name_counts.head(3)
-print(top_three_names)
